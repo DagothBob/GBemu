b00f4ab26649eb6c630ae026a50cf946c611b2d6
Holger <bo3b@live.com>
2020-04-16 22:53:52 -0700

    namespace emu, sdl window init

--- main.cpp
+++ main.cpp
@@ -5,6 +5,13 @@
 #include "SDL2/SDL.h"
 #include "SDL2/SDL_video.h"
 
+namespace emu {
+    void initDisplay();
+    void handleEvents();
+
+    bool quit;
+}
+
 int main(int argc, char** argv) {
     //////////////////////////////
     // SDL initialization stuff //
@@ -12,16 +19,54 @@ int main(int argc, char** argv) {
     if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
         std::cout << "Error initializing SDL: " << SDL_GetError();
     }
-    SDL_Window* window = SDL_CreateWindow("GBemu",
-                                          SDL_WINDOWPOS_CENTERED, 
-                                          SDL_WINDOWPOS_CENTERED,
-                                          640, 576, 0);
+    emu::initDisplay();
     
     ///////////////
     // Main loop //
     ///////////////
-    while (true)
+    emu::quit = false;
+
+    while (!emu::quit)
     {
+        emu::handleEvents();
     }
     return 0;
+}
+
+////////////////////////////
+// Initialize SDL display //
+////////////////////////////
+void emu::initDisplay() {
+    SDL_Window* window = SDL_CreateWindow("GBemu",
+                                        SDL_WINDOWPOS_CENTERED, 
+                                        SDL_WINDOWPOS_CENTERED,
+                                        640, 576, 0);
+    SDL_Surface* windowSurface = NULL;
+
+    if (window == NULL) {
+        std::cout << "Error initializing window: " << SDL_GetError();
+    }
+    else {
+        windowSurface = SDL_GetWindowSurface(window);
+    }
+}
+
+/////////////////////////
+// Part of main loop.  //
+// Handles input, etc. //
+/////////////////////////
+void emu::handleEvents() {
+    SDL_Event e;
+
+    // SDL events //
+    while (SDL_PollEvent(&e) != 0) {
+        switch (e.type)
+        {
+        case SDL_QUIT:
+            emu::quit = true;
+            break;
+        default:
+            break;
+        }
+    }
 }
