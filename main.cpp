1da0e800a1df0ba6a17b4be210e38220289a72ea
Holger <bo3b@live.com>
2020-04-16 21:27:19 -0700

    SDL2 includes work properly now

--- main.cpp
+++ main.cpp
@@ -3,7 +3,25 @@
 
 // SDL libraries
 #include "SDL2/SDL.h"
+#include "SDL2/SDL_video.h"
 
-int main() {
-    std::cout << "Cool";
+int main(int argc, char** argv) {
+    //////////////////////////////
+    // SDL initialization stuff //
+    //////////////////////////////
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cout << "Error initializing SDL: " << SDL_GetError();
+    }
+    SDL_Window* window = SDL_CreateWindow("GBemu",
+                                          SDL_WINDOWPOS_CENTERED, 
+                                          SDL_WINDOWPOS_CENTERED,
+                                          640, 576, 0);
+    
+    ///////////////
+    // Main loop //
+    ///////////////
+    while (true)
+    {
+    }
+    return 0;
 }
