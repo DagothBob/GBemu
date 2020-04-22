a2192b48cc4cf2e0c5e7b0da7b72d362c906aadf
Holger <bo3b@live.com>
2020-04-22 01:07:30 -0700

    memory register init stuff

--- memory.cpp
+++ memory.cpp
@@ -6,11 +6,39 @@
 #include "memory.h"
 
 // Entire memory map (524KB address space)
+//    uint8_t ROMbank0[0x4000];      0x0000 - 0x3FFF
+//    uint8_t ROMbank_sw[0x4000];    0x4000 - 0x7FFF
+//    uint8_t vRAM[0x2000];          0x8000 - 0x9FFF
+//    uint8_t sw_RAM[0x2000];        0xA000 - 0xBFFF
+//    uint8_t RAM[0x2000];           0xC000 - 0xDFFF
+//    uint8_t echo_RAM[0x1E00];      0xE000 - 0xFDFF
+//    uint8_t sprite_attrib[0x100];  0xFE00 - 0xFEFF
+//    uint8_t IO_ports[0x80];        0xFF00 - 0xFF7F
+//    uint8_t RAM2[0x80];            0xFF80 - 0xFFFF
 struct Memory::memoryMap memory_map;
 
 // Initialize memory to boot-up state
 Memory::Memory() {
     fill_zeroes(memory_map);
+    set_memory(0xFF10, 0x80);
+    set_memory(0xFF11, 0x88);
+    set_memory(0xFF12, 0xF3);
+    set_memory(0xFF14, 0xBF);
+    set_memory(0xFF16, 0x3F);
+    set_memory(0xFF19, 0xBF);
+    set_memory(0xFF1A, 0x7F);
+    set_memory(0xFF1B, 0xFF);
+    set_memory(0xFF1C, 0x9F);
+    set_memory(0xFF1E, 0xBF);
+    set_memory(0xFF20, 0xFF);
+    set_memory(0xFF23, 0xBF);
+    set_memory(0xFF24, 0x77);
+    set_memory(0xFF25, 0xF3);
+    set_memory(0xFF26, 0xF1);
+    set_memory(0xFF40, 0x91);
+    set_memory(0xFF47, 0xFC);
+    set_memory(0xFF48, 0xFF);
+    set_memory(0xFF49, 0xFF);
 }
 
 // GB is little-endian
@@ -29,9 +57,15 @@ void Memory::set_memory(uint16_t addr, uint8_t val) {
     }
     else if (addr < 0xE000) {
         memory_map.RAM[addr - 0xC000] = val;
+
+        if ((addr - 0xC000) < sizeof(memory_map.echo_RAM))
+            memory_map.echo_RAM[addr - 0xC000] = val;
     }
     else if (addr < 0xFE00) {
         memory_map.RAM[addr - 0xE000] = val;
+
+        if ((addr - 0xC000) < sizeof(memory_map.echo_RAM))
+            memory_map.echo_RAM[addr - 0xE000] = val;
     }
     else if (addr < 0xFF00) {
         memory_map.sprite_attrib[addr - 0xFE00] = val;
